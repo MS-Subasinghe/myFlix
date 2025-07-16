0a63ac36921e572a66cd5cee5decf84745f3bdad
Minol Subasinghe <159309946+MS-Subasinghe@users.noreply.github.com>
2025-07-17 01:45:38 +0530

    addd blurCirlce and create Featuerd section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // Home.jsx
 import React from 'react';
 import HeroSection from '../components/heroSection.jsx';
+import FeaturedSection from '../components/FeaturedSection.jsx';
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
+      <FeaturedSection/>
     </>
   );
 };
